4bd07cd6e375abefd0fc51d43da1d71f0702bb32
Godomcity <134502441+Godomcity@users.noreply.github.com>
2024-10-30 14:52:01 +0900

    feat : ItemObject

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -3,7 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class ItemObject : MonoBehaviour
+public class ItemObject : MonoBehaviour, IInteractable
 {
     public ItemData data;
+
+    public string GetInteractPrompt()
+    {
+        // 아이템의 이름과 설명을 반환해준다.
+        string str = $"{data.displayName}\n{data.description}";
+        return str;
+    }
+
+    public void OnInteract()
+    {
+        // plyer의 아이템 데이터를 데이테로 만들어준다.
+        // 아이템이 추가되는 이벤트를 실행
+        // 아이템 삭제
+        CharacterManager.Instance.Player.itemData = data;
+        CharacterManager.Instance.Player.addItem?.Invoke();
+        Destroy(gameObject);
+    }
 }
