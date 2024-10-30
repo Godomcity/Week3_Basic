e19ba260f1899ca8e0c5523da8778dab2ec78a65
Godomcity <134502441+Godomcity@users.noreply.github.com>
2024-10-30 14:48:31 +0900

    doce : PlayerScript

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // 외부에서 플레이어의 정보를 가져올 때 Player 스크립트를 접근하여 정보를 가져온다.
-
     public PlayerController controller;
     public PlayerCondition condition;
+
+    // 아이템의 정보와 이벤트 생성
+    public ItemData itemData;
+    public Action addItem;
 
     private void Awake()
     {
